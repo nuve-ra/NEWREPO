e90a9ab443e2e0b5017cd07cbea9fab68f0bc4d2
nuve-ra <nuve19ec@cmrit.ac.in>
2025-03-22 17:03:44 +0530

    commit 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUsers } from '@fortawesome/free-solid-svg-icons';
 import App from './App.jsx';
 import './index.css';
+
+library.add(faUsers);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
